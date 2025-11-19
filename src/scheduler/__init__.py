0a98cc8855aefb2c08a20c2260dc665d8cf187b9
APH <alex-hristov@hotmail.com>
2025-11-19 08:47:36 -0500

    now a python library

--- src/scheduler/__init__.py
+++ src/scheduler/__init__.py
@@ -1,12 +1,11 @@
 """Espresso - A flexible job scheduler library."""
 
 from .scheduler import EspressoScheduler
-from .models import EspressoJobDefinition, EspressoInputDefinition
+from . import models
 from .yaml_loader import load_jobs_from_yaml
 
 __all__ = [
     "EspressoScheduler",
-    "EspressoJobDefinition",
-    "EspressoInputDefinition",
+    "models",
     "load_jobs_from_yaml",
 ]
